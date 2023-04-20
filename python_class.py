c31669cf7ff6d2b69a3dcf7e67e45211bb1ec6f3
Student <student@ms.boyslatin.org>
2023-04-20 09:03:28 -0400

    commit one

---
+++ python_class.py
@@ -0,0 +1,76 @@
+#my_pet_1_type='cat'
+#my_pet_1_noise='meow'
+#my_pet_1_noise='meow'
+#my_pet_1_full_name= 'Snuffles McGruff' 
+
+
+#my_pet_2='pet'
+#my_pet_2_type='cat'
+#my_pet_2_noise='meow'
+#my_pet_2_full_name='Snowpounce Flurry' 
+
+#my_pet_1={
+#'name': 'Snuffles McGruff'   
+#}
+
+#my_pet_2={
+#'name': 'Snowpounce Flurry'   
+#}
+
+
+#print(my_pet_1['name'])
+#print(my_pet_2['name'])
+
+
+#solve for for bugprint(my_pet_1("nam3"))
+
+# my_pets = [my_pet_1, my_pet_2]
+#for pet in my_pets:
+    #print(my_pet_1_full_name,my_pet_2_full_name)
+
+
+
+
+
+
+
+
+
+#class Smoothies:
+    #def __init__ (self, ingrediantA, ingrediantB, ingrediantC):
+     #self.ingrediantA=ingrediantA
+     #self.ingrediantB=ingrediantB
+     #self.ingrediantC=ingrediantC
+
+#order1 = Smoothies ('mango', 'blueberry', 'water ')
+
+#print(order1.ingrediantA)
+#print(order1.ingrediantB)
+#print(order1.ingrediantc)
+
+
+class East_Team:
+    def __init__(self, TeamA, TeamB):
+        self.TeamA=TeamA
+        self.TeamB=TeamB
+        
+
+Easternconference= East_Team('Bucks ', ' Sixers',)
+
+print(Easternconference.TeamA)
+print(Easternconference.TeamB) 
+                                                                                 
+
+class West_team:
+    def   __init__(self, TeamA,TeamB ):
+         self.TeamA=TeamA
+         self.TeamB=TeamB
+
+
+Westernconference= West_team(' Kings', ' Grizzlies')
+
+print(Westernconference.TeamA)
+print(Westernconference.TeamB)
+
+print(East_Team)
+print(West_team)
